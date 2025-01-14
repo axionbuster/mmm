1166629b893b68d95f1313e59060d18953e45093
axionbuster <axionbuster@gmail.com>
2025-01-13 22:23:24 -0800

    Code: generalize to more bit combinations

--- src/M/Chunk/Code.hs
+++ src/M/Chunk/Code.hs
@@ -11,31 +11,37 @@ module M.Chunk.Code (upb, pkb) where
 import Data.Bits
 import Data.Int
 import Data.Word
+import Text.Printf
 
 -- | unpack a paletted container
--- (Minecraft, Java Edition, padded longs).
+-- (Minecraft, Java Edition, padded words).
 --
 -- see also: 'pkb'.
---
--- @1 <= b <= 8@ must hold.
 upb ::
-  forall a.
-  (FiniteBits a, Integral a) =>
+  forall w c.
+  (FiniteBits w, Integral w, FiniteBits c, Integral c) =>
   -- | bits per entry
   Int ->
-  -- | longs; least significant word first.
-  [a] ->
-  -- | octets; least significant octet first.
-  [Word8]
+  -- | words; least significant word first.
+  [w] ->
+  -- | chars; least significant char first.
+  [c]
 upb b = e
   where
+    -- e: entry point / error guard.
+    e
+      | wsz < csz =
+          error $
+            printf
+              "incorrect bit combination in 'upb': wsz = %d, csz = %d"
+              wsz
+              csz
+      | 1 <= b && b <= csz = f c 0
+      | otherwise = error $ "incorrect bits per entry in 'upb': b = " ++ show b
     -- f: extract bits from a word64, skip to next word64 when it should.
     -- case 1: no current word, no more words. halt.
     -- case 2: not enough bits in word; next word, reset state.
     -- case 3: extract bits.
-    e
-      | 1 <= b && b <= 8 = f c 0
-      | otherwise = error $ "incorrect bits per entry in 'upb': b = " ++ show b
     f _ _ [] = []
     f n _ (_ : ws) | n < b = f c 0 ws
     f n s (w : ws)
@@ -45,16 +51,16 @@ upb b = e
     -- case 1: not enough bits.
     -- case 2: ok, extract bits.
     g w s
-      | s + b > bsz = Nothing
+      | s + b > wsz = Nothing
       | otherwise = Just $ (w .&. shift m s) .>>. s
     -- c: how many complete bit groups fit in a word.
-    c = b * div bsz b
+    c = b * div wsz b
     -- m: mask; 0...01...1 <- 'b' count of 1's.
     m = shift 1 b - 1
-    -- bsz: bit size. expected to be 64.
-    bsz = finiteBitSize (undefined :: a)
-    -- fi: take 8 least significant bits from a word.
-    fi = fromIntegral @a @Word8
+    wsz = finiteBitSize (undefined :: w)
+    csz = finiteBitSize (undefined :: c)
+    -- fi: take least significant bits from a word.
+    fi = fromIntegral @w @c
 {-# SPECIALIZE upb :: Int -> [Int64] -> [Word8] #-}
 {-# SPECIALIZE upb :: Int -> [Word64] -> [Word8] #-}
 
@@ -71,36 +77,40 @@ upb b = e
 -- @[0x0084210842108421 .. (10 times)]@
 --
 -- while being strict on the values to prevent thunk buildup.
---
--- @1 <= b <= 8@ must hold.
 pkb ::
-  forall a.
-  (FiniteBits a, Integral a) =>
+  forall w c.
+  (FiniteBits w, Integral w, FiniteBits c, Integral c) =>
   -- | bits per entry
   Int ->
-  -- | octets; least significant octet first.
-  [Word8] ->
-  -- | longs; least significant long first.
-  [a]
-pkb b = c
+  -- | chars; least significant char first.
+  [c] ->
+  -- | words; least significant word first.
+  [w]
+pkb b = k
   where
-    -- c (check): very special initial condition check.
+    -- k (check): very special initial condition check.
     -- empty list input is degenerate anyway, but, if
     -- you do this, you can have the identity upb b (pkb b xs) == xs
     -- for all inputs, including [].
-    c [] = [] -- prevents returning [0] when [] was given as input.
-    c os =
-      if 1 <= b && b <= 8
-        then f 0 0 os -- entry point.
-        else error $ "incorrect bits per entry in 'pkb': b = " ++ show b
-    -- f: pack octet into current word or else skip to next word.
-    -- case 1: there are no octets left. produce word and quit.
-    -- case 2: put octet if possible; reset & produce word if not.
+    k [] = [] -- prevents returning [0] when [] was given as input.
+    k os
+      | wsz < csz =
+          error $
+            printf
+              "incorrect bit combination in 'pkb': wsz = %d, csz = %d"
+              wsz
+              csz
+      | 1 <= b && b <= csz = f 0 0 os
+      | otherwise = error $ "incorrect bits per entry in 'pkb': b = " ++ show b
+    -- f: pack char into current word or else skip to next word.
+    -- case 1: there are no chars left. produce word and quit.
+    -- case 2: put char if possible; reset & produce word if not.
     f _ !w [] = [w]
-    f s !w (o : os)
-      | s + b > bsz = w : f 0 0 (o : os)
-      | otherwise = f (s + b) (w .|. shift (fi o) s) os
-    bsz = finiteBitSize (undefined :: a)
-    fi = fromIntegral @Word8 @a
+    f s !w (c : cs)
+      | s + b > wsz = w : f 0 0 (c : cs)
+      | otherwise = f (s + b) (w .|. shift (fi c) s) cs
+    wsz = finiteBitSize (undefined :: w)
+    csz = finiteBitSize (undefined :: c)
+    fi = fromIntegral @c @w
 {-# SPECIALIZE pkb :: Int -> [Word8] -> [Int64] #-}
 {-# SPECIALIZE pkb :: Int -> [Word8] -> [Word64] #-}
